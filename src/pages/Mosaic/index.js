470627374261455aae94642f1f25f8826c2291d4
Lucas8x <lucas.poring99@gmail.com>
2020-07-31 16:26:21 -0300

    Update index.js

--- src/pages/Mosaic/index.js
+++ src/pages/Mosaic/index.js
@@ -4,42 +4,40 @@ import './styles.css';
 import api from '../../services/mal';
 
 export default function Mosaic({ match }) {
-  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
   const people_id = match.params.id;
 
   useEffect(() => {
-    api.get(`/people/${people_id}`).then(response => {
-      setData(response.data)
-    })
+    async function fetch() {
+      api.get(`/people/${people_id}`).then(response => {
+        setData(response.data);
+        setLoading(false);
+      })
+    }
+    fetch();
   }, [people_id]);
-
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
 
   return (
     <div className="container">
-      {!data.length > 0 ? (
-        <ul>
-          <li key={data.name}>
-            {data.pictures ? (
-              <img src={data.pictures[0]} alt='' />
-            ) : ('')}
-          </li>
-          {data.characters ? (
-            data.characters.map(character => (
-              character.pictures.length > 0 ? (
-                <li key={character.name}>
-                  <img src={character.pictures[0]} alt='' />
-                </li>
-              ) : ('')
-            ))
-          ) : ('')}
-        </ul>
-      ) : (
-          <div className="empty">Empty</div>
-        )
-      }
+      {!loading ? (
+        data ? (
+          <ul>
+            <li key={data.name}>
+              <img src={data.default} alt='' />
+            </li>
+            {data.characters ? (
+              data.characters.map(character => (
+                character.name && character.pictures ? (
+                  <li key={character.name}>
+                    <img src={character.default} alt='' />
+                  </li>
+                ) : null
+              ))
+            ) : ('NO CHARACTERS')}
+          </ul>
+        ) : ('NO DATA')
+      ) : ('Loading...')}
     </div>
   )
 }
