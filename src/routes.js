347136d9bc8e45749fe60aaa5f1cef1a4f1477c6
Lucas8x <lucas.poring99@gmail.com>
2020-07-30 23:02:55 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Mosaic from './pages/Mosaic'
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
       <Route path="/" exact component={Home} />
       <Route path="/mosaic/:id" component={Mosaic} />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
