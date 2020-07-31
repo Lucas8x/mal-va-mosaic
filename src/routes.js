c4dce897ef80420387eab81636e1982d4e2d7729
Lucas8x <lucas.poring99@gmail.com>
2020-07-31 19:14:46 -0300

    Test routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Mosaic from './pages/Mosaic'
 
 export default function Routes() {
   return (
-    <HashRouter basename="/">
-      <Route path="/" exact component={Home} />
-      <Route path="/mosaic/:id" component={Mosaic} />
-    </HashRouter>
+    process.env.REACT_APP_GH_PAGES_DEPLOY ? (
+      <HashRouter basename="/">
+        <Route path="/" exact component={Home} />
+        <Route path="/mosaic/:id" component={Mosaic} />
+      </HashRouter>
+    ) : (
+        <BrowserRouter>
+          <Route path="/" exact component={Home} />
+          <Route path="/mosaic/:id" component={Mosaic} />
+        </BrowserRouter>
+      )
   )
 }
--- src/services/mal.js
+++ src/services/mal.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://mal-people-api.herokuapp.com/'
+  baseURL: process.env.REACT_APP_USE_LOCAL_API
+    ? process.env.REACT_APP_LOCAL_API
+    : 'https://mal-people-api.herokuapp.com/'
 });
