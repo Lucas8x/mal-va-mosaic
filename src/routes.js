40f0b739784883526a91490ca40e31c40997ae88
Lucas8x <lucas.poring99@gmail.com>
2020-08-01 09:48:45 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Mosaic from './pages/Mosaic'
 
 export default function Routes() {
   return (
-    process.env.REACT_APP_GH_PAGES_DEPLOY ? (
-      <HashRouter basename="/">
-        <Route path="/" exact component={Home} />
-        <Route path="/mosaic/:id" component={Mosaic} />
-      </HashRouter>
-    ) : (
-        <BrowserRouter>
-          <Route path="/" exact component={Home} />
-          <Route path="/mosaic/:id" component={Mosaic} />
-        </BrowserRouter>
-      )
+    <HashRouter basename="/">
+      <Route path="/" exact component={Home} />
+      <Route path="/mosaic/:id" component={Mosaic} />
+    </HashRouter>
   )
 }
