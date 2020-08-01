c6a97c685a5dc9e445cb74d3d51de700cedace07
Lucas8x <lucas.poring99@gmail.com>
2020-08-01 10:02:57 -0300

    Update mal.js

--- src/services/mal.js
+++ src/services/mal.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const url = parseInt(process.env.REACT_APP_USE_LOCAL_API)
+  ? process.env.REACT_APP_LOCAL_API
+  : 'https://mal-people-api.herokuapp.com/'
+
 export default axios.create({
-  baseURL: process.env.REACT_APP_USE_LOCAL_API
-    ? process.env.REACT_APP_LOCAL_API
-    : 'https://mal-people-api.herokuapp.com/'
+  baseURL: url
 });
